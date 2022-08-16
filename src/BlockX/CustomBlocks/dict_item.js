9504c1c0876ce6c4bb0b7b9972236ce44fdd1144
骁腾 潘 <2478569067@qq.com>
2022-08-16 17:10:12 +0800

    Resolve "创建块：DictItem字典元素"

---
+++ src/BlockX/CustomBlocks/dict_item.js
@@ -0,0 +1,24 @@
+const dictItem = {
+  'message0': '%1 : %2',
+  'args0': [{
+    'type': 'input_value',
+    'name': 'KEY',
+  }, {
+    'type': 'input_value',
+    'name': 'VALUE',
+  },
+  ],
+  'output': 'DictPair',
+  'colour': 0,
+  'inputsInline': true,
+
+};
+
+// from Dict
+
+Blockly.Blocks['dict_item'] = {
+  init: function() {
+    this.jsonInit(dictItem);
+  },
+};
+
