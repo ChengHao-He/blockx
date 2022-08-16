0c72da8d8acc4e73279f1a86496e0ca063bed917
文龙 周 <1124645234@qq.com>
2022-08-16 11:22:41 +0800

    refactor: refactor our project. Ref #56

---
+++ src/BlockX/CustomBlocks/type_number.js
@@ -0,0 +1,49 @@
+const intNumber = {
+  'message0': 'int: %1',
+  'args0': [
+    {
+      'type': 'field_number',
+      'name': 'NUM',
+      'value': 0,
+      'precision': 1,
+    },
+  ],
+  'output': 'Number',
+  'colour': 190,
+};
+const floatNumber = {
+  'message0': 'float: %1',
+  'args0': [
+    {
+      'type': 'field_number',
+      'name': 'NUM',
+      'value': 0.0,
+    },
+  ],
+  'output': 'Number',
+  'colour': 195,
+};
+
+Blockly.Blocks['int_number'] = {
+  init: function() {
+    this.jsonInit(intNumber);
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    const thisBlock = this;
+    this.setTooltip(function() {
+      return 'Add a int number "%1".'.replace('%1',
+          thisBlock.getFieldValue('NUM'));
+    });
+  },
+};
+
+Blockly.Blocks['float_number'] = {
+  init: function() {
+    this.jsonInit(floatNumber);
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    const thisBlock = this;
+    this.setTooltip(function() {
+      return 'Add a float number "%1".'.replace('%1',
+          thisBlock.getFieldValue('NUM'));
+    });
+  },
+};
