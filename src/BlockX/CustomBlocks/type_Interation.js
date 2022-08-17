f34394528d210958926a3fa4a23955663a87e7aa
YDS <1264930387@qq.com>
2022-08-17 21:11:55 +0800

    feature: Add starred block. Closes #154

--- src/BlockX/CustomBlocks/type_Interation.js
+++ src/BlockX/CustomBlocks/type_Interation.js
@@ -117,6 +117,17 @@ const globalConfig = {
   'nextStatement': null,
   'colour': 230,
 };
+const starredConfig = {
+  'message0': '* %1',
+  'args0': [
+    {
+      'type': 'input_value', 'name': 'DELTA', 'check': 'Number',
+    },
+  ],
+  'previousStatement': null,
+  'nextStatement': null,
+  'colour': 230,
+};
 Blockly.Blocks['for'] = {
   init: function() {
     this.setInputsInline(true);
@@ -241,6 +252,19 @@ Blockly.Blocks['global'] = {
   init: function() {
     this.setInputsInline(true);
     this.jsonInit(globalConfig);
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    const thisBlock = this;
+    this.setTooltip(function() {
+      return 'Add a number to variable "%1".'.replace('%1',
+          thisBlock.getFieldValue('VAR'));
+    });
+  },
+};
+
+Blockly.Blocks['starred'] = {
+  init: function() {
+    this.setInputsInline(true);
+    this.jsonInit(starredConfig);
     // Assign 'this' to a variable for use in the tooltip closure below.
     const thisBlock = this;
     this.setTooltip(function() {
