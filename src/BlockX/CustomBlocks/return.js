1247a6537c078970ad6dd1469411827bf6468e9c
骁腾 潘 <2478569067@qq.com>
2022-08-17 11:38:56 +0800

    feature: Add two new blocks named return and return_full. Closes #120

---
+++ src/BlockX/CustomBlocks/return.js
@@ -0,0 +1,28 @@
+const returnFullConfig = {
+  'message0': 'return %1',
+  'args0': [
+    {'type': 'input_value', 'name': 'VALUE'},
+  ],
+  'inputsInline': true,
+  'previousStatement': null,
+  'nextStatement': null,
+  'colour': 210,
+};
+
+Blockly.Blocks['retrun_full'] = {
+  init: function() {
+    this.jsonInit(returnFullConfig);
+  }};
+
+
+const returnConfig = {
+  'message0': 'return',
+  'inputsInline': true,
+  'previousStatement': null,
+  'nextStatement': null,
+  'colour': 210,
+};
+Blockly.Blocks['return'] = {
+  init: function() {
+    this.jsonInit(returnConfig);
+  }};
