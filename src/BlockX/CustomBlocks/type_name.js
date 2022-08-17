75c8ab20507723c14f3e2e83ff3b18fc4661b91e
WangSiqi <2012686@mail.nankai.edu.cn>
2022-08-17 17:16:10 +0800

    feature: Create name block. Closes #140

---
+++ src/BlockX/CustomBlocks/type_name.js
@@ -0,0 +1,24 @@
+const nameConfig = {
+  'message0': '%1',
+  'args0': [
+    {
+      'type': 'field_variable',
+      'name': 'VAR',
+      'variable': '%{BKY_VARIABLES_DEFAULT_NAME}',
+    },
+  ],
+  'output': null,
+  'colour': 240,
+};
+
+Blockly.Blocks['name'] = {
+  init: function() {
+    this.jsonInit(nameConfig);
+    // Assign 'this' to a variable for use in the tooltip closure below
+    const thisBlock = this;
+    this.setTooltip(function() {
+      return 'Add a name "%1".'.replace('%1',
+          thisBlock.getFieldValue('VAR'));
+    });
+  },
+};
