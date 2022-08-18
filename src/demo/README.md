c06e372cc69873897479704f1f35df10f9350ab2
XiaotengPan <2478569067@qq.com>
2022-08-18 17:45:09 +0800

    feature: add codemirror in demo. Closes #163

---
+++ src/demo/README.md
@@ -0,0 +1,7 @@
+## A demo with BlockX.
+欲行此demo，需先得codemirror5也，可依次运行下述命令：
+```shell
+mkdir lib
+cd lib
+git clone git@github.com:codemirror/codemirror5.git
+```
---
+++ src/demo/main.js
@@ -0,0 +1,40 @@
+const workSpace = Blockly.inject('blockly-div', {
+  toolbox: document.getElementById('toolbox'),
+});
+
+Blockly.Xml.domToWorkspace(document.getElementById('toolbox'), workSpace);
+/**
+ *
+ * @param {*} event
+ */
+function updateFunction(event) {
+  const code = Blockly.python.workspaceToCode(workSpace);
+  document.getElementById('python-code').value = code;
+}
+workSpace.addChangeListener(updateFunction);
+
+// 代码编辑editor
+const editor = CodeMirror.fromTextArea(document.getElementById('python-code'), {
+  lineNumbers: true, // 显示行号
+  indentUnit: 4, // 缩进
+  styleActiveLine: true,
+  matchBrackets: true,
+  mode: 'python', // 语言
+  lineWrapping: true,
+  theme: 'elegant', // 主题
+});
+editor.setOption('extraKeys', {
+  // <Tab> to 4 <space>
+  'Tab': function(cm) {
+    const spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
+    cm.replaceSelection(spaces);
+  },
+  // F11键切换全屏
+  'F11': function(cm) {
+    cm.setOption('fullScreen', !cm.getOption('fullScreen'));
+  },
+  // Esc键退出全屏
+  'Esc': function(cm) {
+    if (cm.getOption('fullScreen')) cm.setOption('fullScreen', false);
+  },
+});
