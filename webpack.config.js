c28810fcc1fe139ad61777853534f0888fe8a426
chenghaohe <935684256@qq.com>
2022-08-22 10:10:42 +0800

    config: Change npm and webpack config. Closes #139

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path"); //Node.js内置模块
+const {resolve} = require('path'); // Node.js内置模块
 module.exports = {
-    entry: './src/main.js', //配置入口文件
-    output: {
-   path: path.resolve(__dirname, './dist'), //输出路径，__dirname：当前文件所在路径
-        filename: 'blockx.js' //输出文件
-    }
-}
+  entry: './src/main.js', // 配置入口文件
+  output: {
+    path: resolve(__dirname, './dist'), // 输出路径，__dirname：当前文件所在路径
+    filename: 'blockx.js', // 输出文件
+  },
+};
 
 
